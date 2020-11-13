be74faadd5b3ee75115bc0290db60c3b3b178fef
mheyn95 <mheyn95@gmail.com>
2020-11-13 11:28:47 -0700

    added for loop to create password, page is fully functional

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,8 +3,10 @@ var upperCaseStr = lowerCaseStr.toUpperCase()
 var numStr = "0123456789"
 var specialCharStr = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~\"\'"
 // Assignment code here
+
 function generatePassword() {
   var passLength = window.prompt("How long do you want your password to be? Please choose a number between 8 and 128");
+  
   if (passLength < 8 || passLength > 128) {
     window.alert("Please try again, invalid length");
     generatePassword();
@@ -12,6 +14,7 @@ function generatePassword() {
   else {
     passLength = parseInt(passLength);
   }
+  
   window.alert("Please select what you would like to include in your password.")
   var lowerCase = window.confirm("Would you like to use lower-case letters in your password?");
   var upperCase = window.confirm("Would you like to use upper-case letters in your password?");
@@ -26,65 +29,155 @@ function generatePassword() {
   else  if (lowerCase && !upperCase && !numPass && !specialChar) {
     //they only want to use lowercase
     var mainList = lowerCaseStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else  if (!lowerCase && upperCase && !numPass && !specialChar) {
     //they only want to use uppercase
     var mainList = upperCaseStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else  if (!lowerCase && !upperCase && numPass && !specialChar) {
     //they only want to use numbers
     var mainList = numStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else  if (!lowerCase && !upperCase && !numPass && specialChar) {
     //they only want to use special characters
     var mainList = specialCharStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else  if (lowerCase && upperCase && !numPass && !specialChar) {
     //they only want to use lowercase and uppercase
     var mainList = lowerCaseStr + upperCaseStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else  if (lowerCase && !upperCase && numPass && !specialChar) {
     //they only want to use lowercase and numbers
     var mainList = lowerCaseStr + numStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else  if (lowerCase && !upperCase && !numPass && specialChar) {
     //they only want to use lowercase and special characters
     var mainList = lowerCaseStr + specialCharStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else  if (!lowerCase && upperCase && numPass && !specialChar) {
     //they only want to use uppercase and numbers
     var mainList = upperCaseStr + numStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else  if (!lowerCase && upperCase && !numPass && specialChar) {
     //they only want to use uppercase and special characters
     var mainList = upperCaseStr + specialCharStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else if (!lowerCase && !upperCase && numPass && specialChar) {
     //they only want to use numbers and special characters
     var mainList = numStr + specialCharStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else if (lowerCase && upperCase && numPass && !specialChar) {
     //they want to use everything but special characters
     var mainList = lowerCaseStr + upperCaseStr + numStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else if (lowerCase && !upperCase && numPass && specialChar) {
     //they want to use everything but uppercase
     var mainList = lowerCaseStr + numStr + specialCharStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else if (lowerCase && upperCase && !numPass && specialChar) {
     //they want to use everything but numbers
     var mainList = lowerCaseStr + upperCaseStr + specialCharStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else if (!lowerCase && upperCase && numPass && specialChar) {
     //they want to use everything but lowercase
     var mainList = upperCaseStr + numStr + specialCharStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   else {
     //they want to use all
     var mainList = lowerCaseStr + upperCaseStr + numStr + specialCharStr;
+    var password = ""
+    for(i=0; i < passLength; i++) {
+      var newChar = Math.floor(Math.random() * mainList.length);
+      newChar = mainList[newChar];
+      password = password + newChar;
+    }
   }
   
-  return mainList;
+  return password;
 }
 
 // Get references to the #generate element
